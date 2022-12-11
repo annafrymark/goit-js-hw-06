e5fd61221318995e0ada2fcde92c8504f66e4661
annafrymark <anna.frymark@gmail.com>
2022-12-11 21:34:53 +0100

    task02 and 03

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const elements = [];
+
+ingredients.forEach(function callback(ingredient, index) {
+  elements.push(document.createElement("li"));
+  elements[index].textContent = ingredient;
+  elements[index].classList.add("item");
+});
+
+document.querySelector("ul#ingredients").append(...elements);
