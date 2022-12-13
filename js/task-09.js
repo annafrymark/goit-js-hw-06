52cc625915e500638cbcb44cf64aa647495865bb
annafrymark <anna.frymark@gmail.com>
2022-12-13 23:05:14 +0100

    task - 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const colorChange = (e) => {
+  let color = getRandomHexColor();
+  document.querySelector("span.color").textContent = color;
+  document.querySelector("body").style.backgroundColor = color;
+};
+
+document
+  .querySelector("button.change-color")
+  .addEventListener("click", colorChange);
